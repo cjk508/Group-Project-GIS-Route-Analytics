6a203860e545571709f97341f1fbcf0e6bc7c707
James White <james_white_d@hotmail.com>
2015-05-26 16:49:42 +0100

    Moved to openlayers 3.5

--- src/app/app.js
+++ src/app/app.js
@@ -6,28 +6,22 @@
  */
 
 // ========= config section ================================================
-var url = '/geoserver/ows?';
+var url = '/geoserver/wfs';
 var center = [-121468.12084883929, 7163110.329270016];
 var zoom = 11;
 // =========================================================================
 
-// override the axis orientation for WMS GetFeatureInfo
-var proj = new ol.proj.Projection({
-  code: 'http://www.opengis.net/gml/srs/epsg.xml#4326',
-  axis: 'enu'
-});
-ol.proj.addEquivalentProjections([ol.proj.get('EPSG:4326'), proj]);
+//var emergencySource = new ol.source.Vector({
+//    url: url,
+//    params: {'LAYERS': "county" + ':' + "details", 'TILED': true},
+//    format: ''
+//});
 
-var emergencySource = new ol.source.TileWMS({
-    url: url,
-    params: {'LAYERS': "county" + ':' + "details", 'TILED': true},
-    serverType: 'geoserver'
+var vectorlayer = new ol.layer.Vector({
+    source: new ol.source.Vector({})
 });
 
 // create the OpenLayers Map object
-// we add a layer switcher to the map with two groups:
-// 1. background, which will use radio buttons
-// 2. default (overlays), which will use checkboxes
 var map = new ol.Map({
   // render the map in the 'map' div
   target: document.getElementById('map'),
@@ -39,11 +33,7 @@ var map = new ol.Map({
       title: 'Street Map',
       source: new ol.source.MapQuest({layer: 'osm'})
     }),
-
-    new ol.layer.Tile({
-      title: "York Emergency Services",
-      source: emergencySource
-    })
+    vectorlayer
   ],
   // initial center and zoom of the map's view
   view: new ol.View({
@@ -51,3 +41,7 @@ var map = new ol.Map({
     zoom: zoom
   })
 });
+
+//map.addInteraction(new ol.interaction.Select({
+//    condition: ol.events.condition.mouseMove
+//}));
