e9461674fe7ad73d354e817713b5c939ba3c144d
James White <james_white_d@hotmail.com>
2015-05-28 12:04:54 +0100

    Cleaned up app.js

--- src/app/app.js
+++ src/app/app.js
@@ -9,13 +9,14 @@
 var url = "/geoserver/ows?";
 var center = [-121468.12084883929, 7163110.329270016];
 var zoom = 11;
+var min_delay = Number.POSITIVE_INFINITY;
+var max_delay = Number.NEGATIVE_INFINITY;
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
 // =========================================================================
 
 var mapLayer = new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: 'osm'})
-});
-
-var greyMapLayer = new ol.layer.Tile({
     source: new ol.source.TileWMS({
         url: url,
         params : {"LAYERS": "county:grayMap"}
@@ -29,7 +30,7 @@ var overviewLayer = new ol.layer.Vector({
     })
 });
 
-var heatmapLayer = new ol.layer.Heatmap({
+var timeHeatmapLayer = new ol.layer.Heatmap({
     source: new ol.source.Vector({
         url: url + "service=WFS&version=2.0.0&request=GetFeature&typeName=county:overview&outputFormat=application/json",
         format: new ol.format.GeoJSON()
@@ -38,71 +39,56 @@ var heatmapLayer = new ol.layer.Heatmap({
     shadow: 500
 });
 
-var min_delay = 9999999;//Number.POSITIVE_INFINITY
-var max_delay = 0; //
-
-function normalise(n){
-	return (n-min_delay)/(max_delay - min_delay)
-}
-
-heatmapLayer.getSource().on('change', function(evt){
-  var source = evt.target;
-  if (source.getState() === 'ready') {
-     source.getFeatures().forEach(function(feature){
-	var delay = feature.get('time_sec_delayed');
-	if(delay< min_delay) min_delay = delay;
-	if(delay > max_delay) max_delay = delay;
+var densityHeatmapLayer = new ol.layer.Heatmap({
+    source: new ol.source.Vector({
+        url: url + "service=WFS&version=2.0.0&request=GetFeature&typeName=county:overview&outputFormat=application/json",
+        format: new ol.format.GeoJSON()
+    })
 });
 
-  }
+timeHeatmapLayer.getSource().on('change', function(evt){
+    var source = evt.target;
+    if (source.getState() === 'ready') {
+        source.getFeatures().forEach(function(feature){
+            var delay = feature.get('time_sec_delayed');
+            if(delay< min_delay) min_delay = delay;
+            if(delay > max_delay) max_delay = delay;
+        });
+
+    }
 });
 
-
-heatmapLayer.getSource().on('addfeature', function(event) {
-  var delay = event.feature.get('time_sec_delayed');
-  event.feature.set('weight', normalise(delay));
-  event.feature.set('radius', delay);
+timeHeatmapLayer.getSource().on('addfeature', function(event) {
+    var delay = event.feature.get('time_sec_delayed');
+    event.feature.set('weight', normalise(delay));
+    event.feature.set('radius', delay);
 });
-
-var container = document.getElementById('popup');
-var content = document.getElementById('popup-content');
-var closer = document.getElementById('popup-closer');
-
-/**
- * Add a click handler to hide the popup.
- * @return {boolean} Don't follow the href.
- */
-closer.onclick = function() {
-  overlay.setPosition(undefined);
-  closer.blur();
-  return false;
-};
 
 /**
  * Create an overlay to anchor the popup to the map.
  */
 var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */ ({
-  element: container,
-  autoPan: true,
-  autoPanAnimation: {
-    duration: 250
-  }
+    element: container,
+    autoPan: true,
+    autoPanAnimation: {
+        duration: 250
+    }
 }));
 
 // create the OpenLayers Map object
 var map = new ol.Map({
-  // render the map in the 'map' div
-  target: document.getElementById('map'),
-  // use the Canvas renderer
-  renderer: 'canvas',
-  //map layers
-  layers: [greyMapLayer, heatmapLayer],
-  // initial center and zoom of the map's view
-  view: new ol.View({
-    center: center,
-    zoom: zoom
-  }),
-  overlays: [overlay]
+    // render the map in the 'map' div
+    target: document.getElementById('map'),
+    // use the Canvas renderer
+    renderer: 'canvas',
+    //map layers
+    layers: [mapLayer, timeHeatmapLayer],
+    // initial center and zoom of the map's view
+    view: new ol.View({
+        center: center,
+        zoom: zoom
+    }),
+    overlays: [overlay]
 });
 
 map.addInteraction(new ol.interaction.Select({
@@ -110,16 +96,30 @@ map.addInteraction(new ol.interaction.Select({
 }));
 
 map.on('singleclick', function(evt) {
-container.style.display = "block";
- map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
-      if (feature) {
-        var geometry = feature.getGeometry();
-        var coord = geometry.getCoordinates();
-	content.innerHTML = "<p>Incident details</p><code>Service Id: " + feature.values_.service_id + "<br /> Trip Id: " +
-	feature.values_.trip_id + "<br /> Distance traveled: " + feature.values_.distance_meters + " meters <br/> Time of dispatch: "+
-	feature.values_.time_dispatch + "<br /> Time Arrival: " + feature.values_.time_arrival + "<br/> Delay: " + 
-	feature.values_.time_sec_delayed + " seconds <br />Vehicle type: ND10 HSL Sembcorp Ford Transit <br /> Staff Count: 2</code>";
-        overlay.setPosition(coord);
-      };
+    container.style.display = "block";
+    map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+        if (feature) {
+            var geometry = feature.getGeometry();
+            var coord = geometry.getCoordinates();
+            content.innerHTML = "<p>Incident details</p><code>Service Id: " + feature.values_.service_id + "<br /> Trip Id: " +
+                feature.values_.trip_id + "<br /> Distance traveled: " + feature.values_.distance_meters + " meters <br/> Time of dispatch: "+
+                feature.values_.time_dispatch + "<br /> Time Arrival: " + feature.values_.time_arrival + "<br/> Delay: " +
+                feature.values_.time_sec_delayed + " seconds <br />Vehicle type: ND10 HSL Sembcorp Ford Transit <br /> Staff Count: 2</code>";
+            overlay.setPosition(coord);
+        }
     });
 });
+
+function normalise(n){
+    return (n-min_delay)/(max_delay - min_delay)
+}
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+ */
+closer.onclick = function() {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+};
