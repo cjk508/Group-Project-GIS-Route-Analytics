9f8017777c772c12e8cc45fe49e90893e4ee0121
Zhivko Georgiev <zg520@york.ac.uk>
2015-06-02 19:23:42 +0100

    Removed tools menu, added reset map button to the map

--- src/app/FunctionExecuteControl.js
+++ src/app/FunctionExecuteControl.js
@@ -16,16 +16,19 @@ app.FunctionExecuteControl = function(opt_options) {
     
   var element = document.createElement('div');
   element.className = 'func-execute ol-unselectable ol-control';
+
   element.appendChild(button);
 
+  
   ol.control.Control.call(this, {
     element: element,
     target: options.target
   });
-
+      element.style.top = options.top;
 };
 
 ol.inherits(app.FunctionExecuteControl, ol.control.Control);
+
 
 $('.func-execute, .ol-attribution button[title]').tooltip({
   placement: 'right'
